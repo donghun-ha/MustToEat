a20579c631a8333a0274e26f3ec692f0179aecf7
tjdduq5433 <tjdduq5433@naver.com>
2024-09-26 10:33:11 +0900

    yeob

--- PythonSpace/user_data.py
+++ PythonSpace/user_data.py
@@ -1,7 +1,135 @@
-from fastapi import APIRouter
-
+from fastapi import FastAPI, File, UploadFile, APIRouter
+from fastapi.responses import FileResponse
+import pymysql
+import os
+import shutil
+'http://127.0.0.1:8000/user/insert?id=123&password=11&name=44&phone=4555&address=qqqq&email=rrrr'
 router = APIRouter()
 
 @router.get("/")
 async def read_items():
     return {'message':'Read all items'}
+
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER) :
+    os.makedirs(UPLOAD_FOLDER)
+
+def connect() :
+    conn = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='qwer1234',
+        db='must_eat',
+        charset='utf8'
+    )
+    return conn
+
+@router.get("/insert")
+async def insert(id: str=None, password: str=None, name: str=None, phone: str=None, address: str=None, email: str=None) :
+    conn = connect()
+    curs = conn.cursor()
+    print(phone)
+    try :
+        sql = "insert into user(id, password, name, phone, address, email) values (%s,%s,%s,%s,%s,%s)"
+        curs.execute(sql, (id, password, name, phone, address, email))
+        conn.commit()
+        conn.close()
+        return {'result' : 'Cool'}
+    except Exception as e :
+        conn.close()
+        print("Error :", e)
+        return {'result' : 'Noo'}
+    
+# @router.get("/nsame")
+# async def nsame
+    
+@router.get("/update")
+async def update(id: str=None, password: str=None, name: str=None, phone: str=None, address: str=None, email: str=None) :
+    conn = connect()
+    curs = conn.cursor()
+
+    try :
+        sql = "update user set password=%s, name=%s, phone=%s, address=%s, email=%s where id=%s"
+        curs.execute(sql, (password, name, phone, address, email, id))
+        conn.commit()
+        conn.close()
+        return {'result' : 'Cool'}
+    except Exception as e :
+        conn.close()
+        print("Error :", e)
+        return {'result' : 'Noo'}
+    
+@router.get("/updateAll")
+async def updateAll(id: str=None, password: str=None, name: str=None, phone: str=None, address: str=None, email: str=None, image: str=None) :
+    conn = connect()
+    curs = conn.cursor()
+
+    try :
+        sql = "update user set password=%s, name=%s, phone=%s, address=%s, email=%s, image=%s where id=%s"
+        curs.execute(sql, (password, name, phone, address, email, image, id))
+        conn.commit()
+        conn.close()
+        return {'result' : 'Cool'}
+    except Exception as e :
+        conn.close()
+        print("Error :", e)
+        return {'result' : 'Error'}
+    
+@router.post("/upload")
+async def upload_file(file:UploadFile=File(...)) :
+    try :
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        with open(file_path, "wb") as better :
+            shutil.copyfileobj(file.filename, better)
+        return {'result' : 'Cool'}
+    except Exception as e :
+        print("Error :", e)
+        return ({'result' : 'Error'})
+    
+@router.get("/select")
+async def select(id: str=None) :
+    conn = connect()
+    curs = conn.cursor()
+
+    sql = 'select id, password, name, phone, address, email, image from user where id=%s'
+    curs.execute(sql, (id))
+    rows = curs.fetchall()
+    conn.close()
+    print(rows)
+
+    return {'result' : rows}
+
+@router.get("/view/{file_name}")
+async def get_file(file_name: str) :
+    file_path = os.path.join(UPLOAD_FOLDER, file_name)
+    if os.path.exists(file_path) :
+        return FileResponse(path = file_path, filename=file_name)
+    return {"result" : 'Noo'}
+
+@router.delete("deleteFile/{file_name}")
+async def delete_file(file_name: str) :
+    try :
+        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+        if os.path.exists(file_path) :
+            os.remove(file_path) 
+        return {'result' : 'OK'}
+    except Exception as e :
+        print("Error", e)
+        return {'result' : 'Noo'}
+    
+@router.get("/delete")
+async def delete(id: str=None) :
+    conn = connect()
+    curs = conn.cursor()
+
+    try :
+        sql = "delete from user where id = %s"
+        curs.execute(sql, (id))
+        conn.commit()
+        conn.close()
+        return {'results' : 'Cool'}
+    except Exception as e :
+        conn.close()
+        print('Error :', e)
+        return {'results' : 'Error'}
+
