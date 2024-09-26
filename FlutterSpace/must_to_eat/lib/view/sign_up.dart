8c118fe356cc530feac9da1a869a4e139249639d
nnmmnn15 <shals1645@naver.com>
2024-09-26 14:40:20 +0900

    add signUp DB2

--- FlutterSpace/must_to_eat/lib/view/sign_up.dart
+++ FlutterSpace/must_to_eat/lib/view/sign_up.dart
@@ -24,6 +24,14 @@ class _SignUpState extends State<SignUp> {
   TextEditingController phoneController = TextEditingController();
   TextEditingController addressController = TextEditingController();
 
+  String? idError;
+  String? passwordError;
+  String? passwordConfirmError;
+  String? emailError;
+  String? nameError;
+  String? phoneError;
+  String? addressError;
+
   UserHandler handler = UserHandler();
 
   @override
@@ -169,36 +177,63 @@ class _SignUpState extends State<SignUp> {
 
   // --- Function ---
   signUpAction() async {
-    if (idController.text.trim().isNotEmpty &&
-        passwordController.text.trim().isNotEmpty &&
-        passwordConfrimController.text.trim().isNotEmpty &&
-        nameController.text.trim().isNotEmpty &&
-        phoneController.text.trim().isNotEmpty &&
-        addressController.text.trim().isNotEmpty &&
-        emailController.text.trim().isNotEmpty) {
-      if (passwordController.text.trim() ==
-          passwordConfrimController.text.trim()) {
-        User user = User(
-          id: idController.text.trim(),
-          password: passwordController.text.trim(),
-          name: nameController.text.trim(),
-          phone: phoneController.text.trim(),
-          address: addressController.text.trim(),
-          email: emailController.text.trim(),
-        );
-        bool result = await handler.insertJSONData(user);
+    // idController.text.trim().isNotEmpty &&
+    // passwordController.text.trim().isNotEmpty &&
+    // passwordConfrimController.text.trim().isNotEmpty &&
+    // nameController.text.trim().isNotEmpty &&
+    // phoneController.text.trim().isNotEmpty &&
+    // addressController.text.trim().isNotEmpty &&
+    // emailController.text.trim().isNotEmpty
+    if (idController.text.trim().isEmpty) {
+      idError = '아이디를 입력해주세요!';
+    } else {
+      // 아이디 중복
 
-        // 회원 가입 성공시 true
-        if (result) {
-          // 알람창
-          print('성공');
-          Get.back();
-        } else {
-          // 실패시
-          // 알람창
-          print('실패');
-        }
+      // 중복이 아닐때
+    }
+    passwordError =
+        passwordController.text.trim().isEmpty ? '비밀번호를 입력해주세요!' : null;
+    passwordConfirmError =
+        passwordConfrimController.text.trim() != passwordController.text.trim()
+            ? '비밀번호가 일치하지 않습니다!'
+            : null;
+    nameError = nameController.text.trim().isEmpty ? '이름을 입력해주세요!' : null;
+    phoneError = phoneController.text.trim().isEmpty ? '전화번호를 입력해주세요!' : null;
+    addressError = addressController.text.trim().isEmpty ? '주소를 입력해주세요!' : null;
+    emailError = emailController.text.trim().isEmpty ? '이메일을 입력해주세요!' : null;
+
+    if (passwordError != null ||
+        idError != null ||
+        passwordConfirmError != null ||
+        nameError != null ||
+        phoneError != null ||
+        addressError != null ||
+        emailError != null) {
+      // 회원가입 실패
+      //
+      print('실패2ß');
+    } else {
+      // 회원가입
+      User user = User(
+        id: idController.text.trim(),
+        password: passwordController.text.trim(),
+        name: nameController.text.trim(),
+        phone: phoneController.text.trim(),
+        address: addressController.text.trim(),
+        email: emailController.text.trim(),
+      );
+      bool result = await handler.insertJSONData(user);
+
+      // 회원 가입 성공시 true
+      if (result) {
+        // 알람창
+        print('성공');
+        Get.back();
+      } else {
+        // 실패시
+        // 알람창
+        print('실패');
       }
     }
   }
-}// End
+} // End
