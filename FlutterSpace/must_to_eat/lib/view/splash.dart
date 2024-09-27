ee5547584b50b8928a137846c7aabbe631d05aa8
nnmmnn15 <shals1645@naver.com>
2024-09-27 10:43:11 +0900

    add splash animation

--- FlutterSpace/must_to_eat/lib/view/splash.dart
+++ FlutterSpace/must_to_eat/lib/view/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:must_to_eat/view/login.dart';
+import 'package:lottie/lottie.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -9,17 +10,32 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashState extends State<Splash> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
-    _navigateToHome();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _navigateToHome();
+      }
+    });
   }
 
-  // 3초 후에 메인 화면으로 이동
-  void _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 2));
-    Get.off(() => const Login()); // Get.to 대신 Get.off 사용
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _navigateToHome() {
+    Get.off(() => const Login());
   }
 
   @override
@@ -27,15 +43,34 @@ class _SplashState extends State<Splash> {
     return Scaffold(
       body: Stack(
         children: [
+          // Background image
           Positioned.fill(
             child: Image.asset(
               'images/back.png',
-              fit: BoxFit.cover, // 이미지를 전체 영역에 맞게 조정
+              fit: BoxFit.cover,
             ),
           ),
-          Center(
+
+          Positioned(
+            top: 120,
+            right: 120,
             child: Image.asset(
               'images/logo.png',
+              width: 200,
+            ),
+          ),
+          // Lottie animation in the center
+          Center(
+            child: Lottie.asset(
+              'images/foodie.json',
+              controller: _controller,
+              width: 300,
+              height: 300,
+              onLoaded: (composition) {
+                _controller
+                  ..duration = composition.duration
+                  ..forward();
+              },
             ),
           ),
         ],
